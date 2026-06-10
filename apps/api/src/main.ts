d139de620cab6b78ee432cc70fe2febc756c1288
mariafiw <mariapssa@fiwtec.com.br>
2026-06-10 09:05:54 -0300

    Alteração a porta usada

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -29,8 +29,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  const port = process.env.API_PORT ?? 3001;
-  await app.listen(port);
+  const port = Number(process.env.PORT ?? process.env.API_PORT ?? 3001);
+await app.listen(port, '0.0.0.0');
   console.log(`API rodando em http://localhost:${port}`);
   console.log(`Swagger em http://localhost:${port}/docs`);
 }
